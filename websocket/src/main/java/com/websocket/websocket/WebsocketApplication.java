bacd193e5cc7abc93ef3479c64ef307d2de05aa1
TaoF <898651231@qq.com>
2020-02-15 13:18:15 +0800

    添加启动提示

--- websocket/src/main/java/com/websocket/websocket/WebsocketApplication.java
+++ websocket/src/main/java/com/websocket/websocket/WebsocketApplication.java
@@ -8,6 +8,9 @@ public class WebsocketApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebsocketApplication.class, args);
+        System.out.println("----------------");
+        System.out.println("项目启动成功");
+        System.out.println("----------------");
     }
 
 }
